5f2bdb9ebfa5e035c72a12d0e97da3a70371fba4
Kyle Czajkowski <skiroyjenkins@gmail.com>
2019-01-09 09:59:59 -0700

    moving to new repo got router and semantic installed, folders set up

---
+++ src/components/main/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import MobileNav from '../nav/MobileNav'
+class Main extends Component {
+  render() {
+    return (
+      <div className="App">
+      <MobileNav />
+      </div>
+    );
+  }
+}
+
+export default Main;
